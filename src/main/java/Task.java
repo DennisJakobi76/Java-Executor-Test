ddb12e52210e3247c7f269d980ab6db95702783b
Dennis Jakobi <dennis.jakobi76@gmail.com>
2026-01-02 16:30:40 +0100

    Implement ExecutorService example and add Task class for concurrent execution

---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task implements  Runnable{
+
+    private String taskName;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("Executing Task: " + taskName + " by Thread: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(taskName+" is completed.");
+    }
+}
